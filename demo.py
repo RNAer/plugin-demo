39c577802fcc957e85f7af9beed0ce910bac4731
Zech Xu <zhenjiang.xu@gmail.com>
2017-03-23 19:14:37 -0700

    add more functions

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
-from pkg_resources import iter_entry_points
+from pkg_resources import iter_entry_points, working_set, Environment, add_activation_listener
+from pprint import pprint
 for i, entry_point in enumerate(iter_entry_points(group='img.plugin', name=None), 1):
     name = entry_point.name
     module = entry_point.module_name
@@ -8,3 +9,8 @@ for i, entry_point in enumerate(iter_entry_points(group='img.plugin', name=None)
     print('entry point module: %s' % module)
     print('entry point object: %r' % f)
     print('the object is a function, run it: %r' % f())
+
+print('================')
+
+plgs = working_set.find_plugins(Environment())
+pprint(plgs)
